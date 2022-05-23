a23a3f1ddf9e73522a85406e03cae132b7dc7389
lavenderlover <81465060+lavenderlover@users.noreply.github.com>
2022-05-24 00:28:18 +0800

    Update NowCoder0522Demo1.java
    循环

--- NowCoder0522Demo1.java
+++ NowCoder0522Demo1.java
@@ -5,6 +5,45 @@
 public class NowCoder0522Demo1 {
 
     public static int[][] generateMatrix(int n) {
+        int maxNum = n * n;
+        int[][] matrix = new int[n][n];
+        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
+        int num = 1;
+        matrix[0][0] = num;
+        int dirIndex = 0;
+        int x = 0;
+        int y = 0;
+        int[] dir = dirs[dirIndex];
+        while (++num <= maxNum) {
+            if (isValidPosition(x, y, dir, n) || matrix[x + dir[0]][y + dir[1]] != 0) {
+                dirIndex = dirIndex == 3 ? 0 : dirIndex + 1;
+                dir = dirs[dirIndex];
+            }
+            x += dir[0];
+            y += dir[1];
+            matrix[x][y] = num;
+            if (num == maxNum) {
+                break;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static boolean isValidPosition(int x, int y, int[] dir, int n) {
+        return x + dir[0] >= n || y + dir[1] >= n || x + dir[0] < 0 || y + dir[1] < 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int[][] matrix = generateMatrix(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + (j == n - 1 ? "" : ","));
+            }
+            System.out.println();
+        }
     }
 }
