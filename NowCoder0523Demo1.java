9edd9b7afbb3f848726e85e55a8cd96749303ca4
lavenderlover <81465060+lavenderlover@users.noreply.github.com>
2022-05-24 00:34:40 +0800

    Update NowCoder0523Demo1.java
    ok

--- NowCoder0523Demo1.java
+++ NowCoder0523Demo1.java
@@ -3,4 +3,18 @@
  */
 public class NowCoder0523Demo3 {
     
+    // 766. 托普利茨矩阵
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int r = matrix.length, c = matrix[0].length;
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                // 每一个数与它的左上角数相比较
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
 }
